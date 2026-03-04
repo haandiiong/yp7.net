9343aba6d80966946206365765c596cf6561f987
haandiiong <56588109+haandiiong@users.noreply.github.com>
2026-03-04 23:38:51 +0000

    修改文章

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -6,8 +6,31 @@ export default defineUserConfig({
   lang: 'zh-CN',
   head: [
     // ['link', { rel: 'icon', href: '/images/favicon.ico' }],
-    ["meta", {"name": "keywords", "content": "机场,便宜机场,梯子,vpn,科学上网,翻墙,clash,trojan,python,服务器"}],
-    ["meta", {"name": "description", "content": "开发、运维、科学上网相关内容，打破技术壁垒"}],
+    ['meta', { charset: 'utf-8' }],
+    ['meta', { name: 'viewport', content: 'width=device-width, initial-scale=1' }],
+    ['meta', { name: 'robots', content: 'noindex, follow' }],
+
+    ['meta', {
+      name: 'keywords',
+      content: 'VPN推荐,机场推荐,科学上网教程,翻墙教程,低延迟VPN,节点测速'
+    }],
+
+    ['link', { rel: 'canonical', href: 'https://yp7.net/' }],
+
+    // OpenGraph
+    ['meta', { property: 'og:title', content: 'yp7 - VPN与机场推荐' }],
+    ['meta', { property: 'og:description', content: '稳定机场评测、科学上网完整教程与节点测速。' }],
+    ['meta', { property: 'og:type', content: 'website' }],
+    ['meta', { property: 'og:url', content: 'https://yp7.net/' }],
+    ['meta', { property: 'og:site_name', content: 'yp7.net' }],
+    ['meta', { property: 'og:locale', content: 'zh_CN' }],
+    ['meta', { property: 'og:image', content: 'https://yp7.net/image.png' }],
+
+    // Twitter
+    ['meta', { name: 'twitter:card', content: 'summary_large_image' }],
+    ['meta', { name: 'twitter:title', content: 'yp7.net - 2026机场推荐指南' }],
+    ['meta', { name: 'twitter:description', content: '最新VPN推荐、机场评测与低延迟节点测速。' }],
+    ['meta', { name: 'twitter:image', content: 'https://yp7.net/image.png' }],
   ],
   theme: plumeTheme({
     // logo: '/images/logo.svg',
@@ -19,19 +42,19 @@ export default defineUserConfig({
 
     ],
     profile: {
-        name: 'CFF',
-        description: '一个神秘的IT程序猿',
-        // avatar: '/images/logo.svg',
-      },
+      name: 'CFF',
+      description: '一个神秘的IT程序猿',
+      // avatar: '/images/logo.svg',
+    },
     social: [
-        { icon: 'github', link: 'https://github.com/haandiiong' },
-        {
-          icon: {svg: '<svg viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M41.4193 7.30899C41.4193 7.30899 45.3046 5.79399 44.9808 9.47328C44.8729 10.9883 43.9016 16.2908 43.1461 22.0262L40.5559 39.0159C40.5559 39.0159 40.3401 41.5048 38.3974 41.9377C36.4547 42.3705 33.5408 40.4227 33.0011 39.9898C32.5694 39.6652 24.9068 34.7955 22.2086 32.4148C21.4531 31.7655 20.5897 30.4669 22.3165 28.9519L33.6487 18.1305C34.9438 16.8319 36.2389 13.8019 30.8426 17.4812L15.7331 27.7616C15.7331 27.7616 14.0063 28.8437 10.7686 27.8698L3.75342 25.7055C3.75342 25.7055 1.16321 24.0823 5.58815 22.459C16.3807 17.3729 29.6555 12.1786 41.4193 7.30899Z"></path> </g></svg>'},
-          link: 'https://t.me/cff66778'
-        },
-      ],
+      { icon: 'github', link: 'https://github.com/haandiiong' },
+      {
+        icon: { svg: '<svg viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M41.4193 7.30899C41.4193 7.30899 45.3046 5.79399 44.9808 9.47328C44.8729 10.9883 43.9016 16.2908 43.1461 22.0262L40.5559 39.0159C40.5559 39.0159 40.3401 41.5048 38.3974 41.9377C36.4547 42.3705 33.5408 40.4227 33.0011 39.9898C32.5694 39.6652 24.9068 34.7955 22.2086 32.4148C21.4531 31.7655 20.5897 30.4669 22.3165 28.9519L33.6487 18.1305C34.9438 16.8319 36.2389 13.8019 30.8426 17.4812L15.7331 27.7616C15.7331 27.7616 14.0063 28.8437 10.7686 27.8698L3.75342 25.7055C3.75342 25.7055 1.16321 24.0823 5.58815 22.459C16.3807 17.3729 29.6555 12.1786 41.4193 7.30899Z"></path> </g></svg>' },
+        link: 'https://t.me/cff66778'
+      },
+    ],
     plugins: {
-     
+
     },
     markdown: {
       collapse: true,
