cbcd64c2166c80050a08ce0caacb05001cf808ac
haandiiong <56588109+haandiiong@users.noreply.github.com>
2026-03-08 22:45:08 +0000

    修改config2

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -8,7 +8,6 @@ export default defineUserConfig({
     // ['link', { rel: 'icon', href: '/images/favicon.ico' }],
     ['meta', { charset: 'utf-8' }],
     ['meta', { name: 'viewport', content: 'width=device-width, initial-scale=1' }],
-    ['meta', { name: 'robots', content: 'index, follow' }],
 
     ['meta', {
       name: 'keywords',
