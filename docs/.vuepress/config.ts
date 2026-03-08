f081aabd94482e64773957e37c4005aa3806398c
haandiiong <56588109+haandiiong@users.noreply.github.com>
2026-03-08 22:41:34 +0000

    修改config

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -8,7 +8,7 @@ export default defineUserConfig({
     // ['link', { rel: 'icon', href: '/images/favicon.ico' }],
     ['meta', { charset: 'utf-8' }],
     ['meta', { name: 'viewport', content: 'width=device-width, initial-scale=1' }],
-    ['meta', { name: 'robots', content: 'noindex, follow' }],
+    ['meta', { name: 'robots', content: 'index, follow' }],
 
     ['meta', {
       name: 'keywords',
