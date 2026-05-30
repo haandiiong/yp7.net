809bc00c65dcb3cc0290f4b86cbdd619fafc82c0
haandiiong <haandiiong@gmail.com>
2026-05-30 21:18:35 +0800

    修改排名

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,6 +1,7 @@
 import { defineUserConfig } from 'vuepress'
 import { viteBundler } from '@vuepress/bundler-vite'
 import { plumeTheme } from 'vuepress-theme-plume'
+// import  mediumZoomPlugin from '@vuepress/plugin-medium-zoom'
 
 export default defineUserConfig({
   lang: 'zh-CN',
